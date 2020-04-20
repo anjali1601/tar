75a83a8168a350d17982738130da08386084b522
anjali1601 <32664690+anjali1601@users.noreply.github.com>
2020-04-20 10:59:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,65 @@
-print ("hello")
+subscription_key = 'b2f15e6aaf9e4dc8a0186a5fe7338f1a' 
+endpoint = 'https://eastasia.api.cognitive.microsoft.com/anomalydetector/v1.0/timeseries/entire/detect'
+import requests
+import json
+import pandas as pd
+import numpy as np
+from __future__ import print_function
+import warnings
+warnings.filterwarnings('ignore')
+
+# Import library to display results
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure,output_notebook, show
+from bokeh.palettes import Blues4
+from bokeh.models import ColumnDataSource,Slider
+import datetime
+from bokeh.io import push_notebook
+from dateutil import parser
+from ipywidgets import interact, widgets, fixed
+def detect(endpoint, subscription_key, request_data):
+    headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': subscription_key}
+    response = requests.post(endpoint, data=json.dumps(request_data), headers=headers)
+    if response.status_code == 200:
+        return json.loads(response.content.decode("utf-8"))
+    else:
+        print(response.status_code)
+        raise Exception(response.text)
+        
+def build_figure(sample_data, sensitivity):
+    sample_data['sensitivity'] = sensitivity
+    result = detect(endpoint, subscription_key, sample_data)
+    columns = {'expectedValues': result['expectedValues'], 'isAnomaly': result['isAnomaly'], 'isNegativeAnomaly': result['isNegativeAnomaly'],
+          'isPositiveAnomaly': result['isPositiveAnomaly'], 'upperMargins': result['upperMargins'], 'lowerMargins': result['lowerMargins'],
+          'timestamp': [parser.parse(x['timestamp']) for x in sample_data['series']], 
+          'value': [x['value'] for x in sample_data['series']]}
+    response = pd.DataFrame(data=columns)
+    values = response['value']
+    label = response['timestamp']
+    anomalies = []
+    anomaly_labels = []
+    index = 0
+    anomaly_indexes = []
+    p = figure(x_axis_type='datetime', title="Batch Anomaly Detection ({0} Sensitvity)".format(sensitivity), width=800, height=600)
+    for anom in response['isAnomaly']:
+        if anom == True and (values[index] > response.iloc[index]['expectedValues'] + response.iloc[index]['upperMargins'] or 
+                         values[index] < response.iloc[index]['expectedValues'] - response.iloc[index]['lowerMargins']):
+            anomalies.append(values[index])
+            anomaly_labels.append(label[index])
+            anomaly_indexes.append(index)
+        index = index+1
+    upperband = response['expectedValues'] + response['upperMargins']
+    lowerband = response['expectedValues'] -response['lowerMargins']
+    band_x = np.append(label, label[::-1])
+    band_y = np.append(lowerband, upperband[::-1])
+    boundary = p.patch(band_x, band_y, color=Blues4[2], fill_alpha=0.5, line_width=1, legend='Boundary')
+    p.line(label, values, legend='Value', color="#2222aa", line_width=1)
+    p.line(label, response['expectedValues'], legend='ExpectedValue',  line_width=1, line_dash="dotdash", line_color='olivedrab')
+    anom_source = ColumnDataSource(dict(x=anomaly_labels, y=anomalies))
+    anoms = p.circle('x', 'y', size=5, color='tomato', source=anom_source)
+    p.legend.border_line_width = 1
+    p.legend.background_fill_alpha  = 0.1
+    show(p, notebook_handle=True)
+sample_data = json.load(open('csvjson.json'))
+sample_data['granularity'] = 'minutely'
+build_figure(sample_data,95)
